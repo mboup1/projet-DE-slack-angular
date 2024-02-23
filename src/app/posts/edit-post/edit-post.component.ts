320f0a8cc94fbe73f0a3b0fb01550fa1d9119c34
mboup <mboup124@gmail.com>
2024-02-24 00:27:17 +0100

    update bootstrap

--- src/app/posts/edit-post/edit-post.component.ts
+++ src/app/posts/edit-post/edit-post.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { API_BASE_URL } from '../../config/config';
+import axios from 'axios';
 
 @Component({
   selector: 'app-edit-post',
@@ -6,5 +10,56 @@ import { Component } from '@angular/core';
   styleUrl: './edit-post.component.css'
 })
 export class EditPostComponent {
+  postForm!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.initPostForm();
+
+    this.route.queryParams.subscribe(params => {
+      this.postForm.patchValue({
+        id: params['id'],
+        idChannel: params['idChannel'],
+        message: params['message'],
+
+
+      });
+      console.log("this.postForm", this.postForm.value);
+
+    });
+  }
+
+
+  initPostForm(): void {
+    this.postForm = this.formBuilder.group({
+      id: null,
+      message: ['', Validators.required],
+      datePoste: [''],
+      idChannel: ['', Validators.required],
+
+    });
+  }
+
+
+
+  updatePost(id: number, updatedPost: any, idChannel: number): void {
+    console.log("id :", id);
+    console.log("idChannel :", idChannel);
+    console.log("updatedPost :", updatedPost);
+
+    axios.put(`${API_BASE_URL}/post/${id}`, updatedPost)
+      .then(response => {
+        console.log("Post updated successfully:", response);
+        this.router.navigate([`/channels/${idChannel}`]);
+      })
+      .catch(error => {
+        console.error("Update failed:", error);
+      });
+  }
 
 }
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Channel } from '../interfaces/channel';
 import { ChannelService } from '../channels/service/channel.service';
 
@@ -9,6 +9,8 @@ import { ChannelService } from '../channels/service/channel.service';
 })
 export class SidebarComponent {
   channels: Channel[] = [];
+  isMobile: boolean = window.innerWidth < 992;
+
 
   constructor(
     private channelService: ChannelService,
@@ -17,11 +19,18 @@ export class SidebarComponent {
   ngOnInit(): void {
     this.channelService.fetchData().subscribe(() => {
       this.channels = this.channelService.getChannels();
-      
+
       this.channels.sort((a, b) => (b.id < a.id) ? 1 : -1);
 
       // console.log("this.channels : ", this.channels)
     });
   }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    this.isMobile = window.innerWidth < 992;
+  }
+
+
 
 }
