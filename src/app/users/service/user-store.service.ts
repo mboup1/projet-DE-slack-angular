38716e6915e09b2489333ec91ecc15f9ed422d26
Eric <larremdy@labom2iformation.fr>
2024-02-26 00:01:34 +0100

    user-store service et list-user-component

---
+++ src/app/users/service/user-store.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { User } from '../../interfaces/user';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserStoreService {
+_users:BehaviorSubject<User[]> = new BehaviorSubject<User[]>([]);
+users$: Observable<User[]> = this._users.asObservable();
+
+ get users(): User[] {
+   return this._users.getValue();
+ }
+
+ set users(val: User[]){
+   this._users.next(val)
+ }
+
+ addUser(newUser: User){
+   this.users = ([...this.users, newUser]);
+ }
+ deleteCLientById(id: number){
+   this.users = this.users.filter(user => user.id !== id );
+ }
+ updateClient(updatedUser: User) {
+   const currentUsers = [...this.users];
+   const index = currentUsers.findIndex(client => client.id === updatedUser.id);
+   currentUsers[index] = updatedUser;
+   this.users = (currentUsers);
+ }
+
+  constructor() { }
+}
+
+
